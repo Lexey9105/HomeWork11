69c30df81f73cf89a463fc6a6b79bc31305bb396
Lexey9105 <umpp120@gmail.com>
2023-03-29 18:23:20 +0300

    Все замечания исправил

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,16 @@ task();
 
     public static void printLeapYear(int i) {
 
-            if (i%4==0&&i%100!=0||i%400==0&&i%4==0) {
+            if (i%4==0&&i%100!=0||i%400==0) {
                 System.out.println(i+" Является високосным");
             }  else {System.out.println(i+" Является невисокосным");}
 
     }
 
     public static void printOS(int i, int y) {
+        if (i > 1 || i < 0) {
+            throw new RuntimeException("Некоректная версия OS");
+        }
         if (i == 0 && y >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (i == 0 && y < 2015) {
@@ -22,33 +25,32 @@ task();
         } else if (i == 1 && y < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
-        if (i > 1 && i < 0) {
-            throw new RuntimeException("Некоректная версия OS");
-        }
+
     }
 
-    public static void printDelivery(int i, int y) {
+    public static int printDelivery(int i, int y) {
         if (i > 0 && i <= 20) {
-            y = y + 1;
-            System.out.println("Потребуется дней: " + y);
+            y = y + 1;System.out.println("Потребуется дней "+y);
+            return y;
+
         }
         if (i > 20 && i < 60) {
-            y = y + 2;
-            System.out.println("Потребуется дней: " + y);
+            y = y + 2;System.out.println("Потребуется дней "+y);
+            return y;
         }
         if (i >= 60 && i < 100) {
-            y = y + 3;
-            System.out.println("Потребуется дней: " + y);
+            y = y + 3;System.out.println("Потребуется дней "+y);
+            return y;
         } else {
-            System.out.println("доставки нет");
+            return 0;
         }
     }
 
     public static void task(){
         int leapYear=1780;
-        int clientOS=0;
+        int clientOS=1;
         int clientDeviceYear=2016;
-        int deliveryDistance=65;
+        int deliveryDistance=45;
         int days=0;
         printLeapYear(leapYear);
         printOS(clientOS,clientDeviceYear);
